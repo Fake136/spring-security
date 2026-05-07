e58fd444fcab3c07292ce2865f728453b884bdfe
Fake <161884694+Fake136@users.noreply.github.com>
2026-05-06 22:19:30 -0300

    Add files via upload

---
+++ spring-security-main/src/main/java/com/spring/security/models/User.java
@@ -0,0 +1,55 @@
+package com.spring.security.models;
+
+import com.spring.security.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "USERS")
+@Data
+public class User implements UserDetails {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   private String login;
+   private String password;
+
+   @Enumerated(EnumType.STRING)
+   private UserRole role;
+
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      if (this.role == UserRole.ADMIN) {
+         return List.of(
+                 new SimpleGrantedAuthority("ROLE_ADMIN"),
+                 new SimpleGrantedAuthority("ROLE_USER")
+         );
+      }
+      return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+   }
+
+   @Override
+   public String getUsername() {
+      return login;
+   }
+
+   @Override
+   public boolean isAccountNonExpired() { return true; }
+
+   @Override
+   public boolean isAccountNonLocked() { return true; }
+
+   @Override
+   public boolean isCredentialsNonExpired() { return true; }
+
+   @Override
+   public boolean isEnabled() { return true; }
+}
